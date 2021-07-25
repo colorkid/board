82670238aaccb90050429705ba4e0c29ae721404
Musatov Yuriy <colorkid@yandex.ru>
2021-07-25 16:43:34 +0300

    Refactor ApiController

--- src/api/ApiController.ts
+++ src/api/ApiController.ts
@@ -14,35 +14,35 @@ export const ApiController = {
     getTasks: async (userId: string): Promise<string | Promise<firebase.database.DataSnapshot>> =>
         !userId
             ? LocalStorageApi.getLocal(TASK_LIST_KEY)
-            : (await firebase.database().ref(`${userId}/tasks`).once('value')).val(),
+            : (await firebase.database().ref(`${userId}/${TASK_LIST_KEY}`).once('value')).val(),
     setTasks: (userId: string, data: DataSetType): void | Promise<null> =>
         !userId
             ? LocalStorageApi.setLocal(TASK_LIST_KEY, data)
-            : firebase.database().ref(`${userId}/tasks`).set(data),
+            : firebase.database().ref(`${userId}/${TASK_LIST_KEY}`).set(data),
     getSprints: async (userId: string): Promise<string | Promise<firebase.database.DataSnapshot>> =>
         !userId
             ? LocalStorageApi.getLocal(SPRINT_LIST_KEY)
-            : (await firebase.database().ref(`${userId}/sprints`).once('value')).val(),
+            : (await firebase.database().ref(`${userId}/${SPRINT_LIST_KEY}`).once('value')).val(),
     setSprints: (userId: string, data: DataSetType): void | Promise<null> =>
         !userId
             ? LocalStorageApi.setLocal(SPRINT_LIST_KEY, data)
-            : firebase.database().ref(`${userId}/sprints`).set(data),
+            : firebase.database().ref(`${userId}/${SPRINT_LIST_KEY}`).set(data),
     getActiveSprint: async (
         userId: string
     ): Promise<string | Promise<firebase.database.DataSnapshot>> =>
         !userId
             ? LocalStorageApi.getLocal(ACTIVE_SPRINT_KEY)
-            : (await firebase.database().ref(`${userId}/activeSprint`).once('value')).val(),
+            : (await firebase.database().ref(`${userId}/${ACTIVE_SPRINT_KEY}`).once('value')).val(),
     setActiveSprint: (userId: string, data: DataSetType): void | Promise<null> =>
         !userId
             ? LocalStorageApi.setLocal(ACTIVE_SPRINT_KEY, data)
-            : firebase.database().ref(`${userId}/activeSprint`).set(data),
+            : firebase.database().ref(`${userId}/${ACTIVE_SPRINT_KEY}`).set(data),
     getColumns: async (userId: string): Promise<string | Promise<firebase.database.DataSnapshot>> =>
         !userId
             ? LocalStorageApi.getLocal(BOARD_COLUMNS_KEY)
-            : (await firebase.database().ref(`${userId}/columns`).once('value')).val(),
+            : (await firebase.database().ref(`${userId}/${BOARD_COLUMNS_KEY}`).once('value')).val(),
     setColumns: (userId: string, data: DataSetType): void | Promise<null> =>
         !userId
             ? LocalStorageApi.setLocal(BOARD_COLUMNS_KEY, data)
-            : firebase.database().ref(`${userId}/columns`).set(data),
+            : firebase.database().ref(`${userId}/${BOARD_COLUMNS_KEY}`).set(data),
 };
